9210ae39cf125c7e6f8dc364ec8ff68568f86350
Rutik5233 <60165077+Rutik5233@users.noreply.github.com>
2025-03-15 00:19:38 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345;
+let accountEmail = "test@gmail.com"
+var accountName = "someName"
+accountCity = "Pune"
+let accountstate;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountName, accountCity, accountstate]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let name = "Rutik"
+let age = 18
+let isLoggedIn = false
+
+/*
+number => 2 to power 53
+BigInt
+string
+boolean => true/false
+null => standalone value
+undefined
+symbol
+*/
+
+//object
+
+console.log(typeof undefined)
